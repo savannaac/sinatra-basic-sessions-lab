ded6b27f105671682b1081735d5bdfc1e9135b02
savanna chiang <savannaac@gmail.com>
2021-03-18 00:26:10 -0500

    complete

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
+
+    get ("/") { erb :index }
+
+    post "/checkout" do
+        @sessions = session
+        item = params["item"]
+        @sessions[:item] = item
+    end
 end
